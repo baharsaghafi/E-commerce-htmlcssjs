84a334472cd071145c4a57d0bb51837d9bd2875e
Bahar Saghafi <bahar.saghafi@gmail.com>
2022-09-30 13:01:33 +0100

    slider Done

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const wrapper = document.querySelector(".slideErapper")
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      //change the current slide
+      wrapper.style.transform = `translateX(${-100 * index}vw)`;  
+    });
+});
