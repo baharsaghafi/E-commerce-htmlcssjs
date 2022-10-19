791d5f6fa54c23e1a7fab8f5ae6b538215317bc4
Bahar Saghafi <bahar.saghafi@gmail.com>
2022-10-19 13:42:30 +0100

    start js product

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
+
+
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
       //change the current slide
